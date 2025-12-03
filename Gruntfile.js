1f9a4a7cfd9b0ae6d90515aebfa5b71a8fa2af90
Jacquelyn Marcella <jacquelyn.marcella@gmail.com>
2025-12-03 10:04:16 -0800

    Get grunt minifying files again, add grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
@@ -8,27 +8,30 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/js/portfolio.js',
-        dest: 'js/portfolio.min.js'
+        expand: true,
+        cwd: 'src/js/',
+        src: ['*.js'],
+        dest: 'js/',
+        ext: '.min.js'
       }
     },
     bake: {
-        build: {
-            files: {
-                "index.html": "src/index.html",
-                "accessibility.html": "src/accessibility.html",
-                "blackjack.html": "src/blackjack.html",
-                "design-system.html": "src/design-system.html",
-                "dreamstate.html": "src/dreamstate.html",
-                "form-api.html": "src/form-api.html",
-                "media-optimization.html": "src/media-optimization.html",
-                "mega-menu-navigation.html": "src/mega-menu-navigation.html",
-                "plant-parenthood.html": "src/plant-parenthood.html",
-                "search-api.html": "src/search-api.html",
-                "skincare.html": "src/skincare.html"
-            }
+      build: {
+        files: {
+          "index.html": "src/index.html",
+          "accessibility.html": "src/accessibility.html",
+          "blackjack.html": "src/blackjack.html",
+          "design-system.html": "src/design-system.html",
+          "dreamstate.html": "src/dreamstate.html",
+          "form-api.html": "src/form-api.html",
+          "media-optimization.html": "src/media-optimization.html",
+          "mega-menu-navigation.html": "src/mega-menu-navigation.html",
+          "plant-parenthood.html": "src/plant-parenthood.html",
+          "search-api.html": "src/search-api.html",
+          "skincare.html": "src/skincare.html"
         }
-    },    
+      }
+    },
     cssmin: {
       options: {
         // Optional: Add a banner to the minified CSS
@@ -40,20 +43,37 @@ module.exports = function(grunt) {
         src: ['*.css', '!*.min.css'], // Match all .css files except already minified ones
         dest: 'css/', // Destination directory for minified CSS
         ext: '.min.css' // Add .min.css extension to minified files
-      },
-      minify: {
-        expand: true,
-        cwd: 'css/', // Source directory for your CSS files
-        src: ['*.css', '!*.min.css'], // Match all .css files except already minified ones
-        dest: 'css/', // Destination directory for minified CSS
-        ext: '.min.css' // Add .min.css extension to minified files
       }
+    },
+    watch: {
+      scripts: {
+        files: ['src/js/*.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
+        },
+      },
+      css: {
+        files: ['src/css/custom/*.css'],
+        tasks: ['cssmin'],
+        options: {
+          spawn: false,
+        },
+      },
+      html: {
+        files: ['src/**/*.html', 'includes/**/*.html'],
+        tasks: ['bake'],
+        options: {
+          spawn: false,
+        },
+      },
     },
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-bake');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default to compile theme updates
   grunt.registerTask('default', ['bake', 'cssmin', 'uglify']);
